d0ec1b6b28315e9e305c264f31dc532a68f1af02
Aldair Balanzar <aldair.balanzar@outlook.com>
2020-02-20 16:47:45 -0500

    added base yugioh styling

--- src/ImgContainer.js
+++ src/ImgContainer.js
@@ -6,30 +6,50 @@ const Card = styled.div`
     flex-wrap:wrap;
     justify-content: center;
     width: 50%;
-    background-color: lightgrey;
+    border: solid 8px transparent;
+    border-radius: 20px;
+    background-image: url(https://i.etsystatic.com/7666691/r/il/46231c/1971450361/il_794xN.1971450361_rkg0.jpg);
+    background-size: contain;
+    border-image: url(https://data.whicdn.com/images/220193202/original.jpg) 30 stretch;
+`;
+
+const HeaderContainer = styled.div `
+    display: flex;
+    width: 100%
+    background-color: rgba(207,180,161,.3);
 `;
 
 const CardTitle = styled.h2 `
     width: 100%;
-    color: grey;
 `;
 
-const CardPar = styled.p `
-    width: 88%;
+const CardDate = styled.h2 `
+    width: 100%;
 `;
 
 const CardImg = styled.img `
     object-fit: scale;
-    width: 90%;
-    padding: 0 0 6%;
+    width: 84%;
+    border: solid 8px transparent;
+    border-image: url(https://data.whicdn.com/images/220193202/original.jpg) 30 stretch;
+`;
+
+const CardPar = styled.p `
+    width: 84%;
+    background-color: rgba(207,180,161,.8);
+    border-radius: 3px;
+    padding: 1%;
 `;
 
 function ImgContainer ({data}) {
     return(
         <Card>
-            <CardTitle>{data.date}</CardTitle>
-            <CardPar>{data.explanation}</CardPar>
+            <HeaderContainer>
+            <CardTitle>{data.title}</CardTitle>
+            <CardDate>{data.date}</CardDate>
+            </HeaderContainer>
             <CardImg src={data.url}/>
+            <CardPar>{data.explanation}</CardPar>
             <p></p>
         </Card>
     )
