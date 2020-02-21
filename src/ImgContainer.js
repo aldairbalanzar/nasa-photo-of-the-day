12553d3eac6862442e6f3a9e47891619b97705a7
Aldair Balanzar <aldair.balanzar@outlook.com>
2020-02-20 19:27:09 -0500

    updated some styling to stars

--- src/ImgContainer.js
+++ src/ImgContainer.js
@@ -8,24 +8,32 @@ const Card = styled.div`
     flex-wrap:wrap;
     justify-content: center;
     width: 44%;
-    border: solid 8px transparent;
+    border: solid 10px transparent;
     background-image: url(https://i.etsystatic.com/7666691/r/il/46231c/1971450361/il_794xN.1971450361_rkg0.jpg);
     background-size: contain;
     border-image: url(https://data.whicdn.com/images/220193202/original.jpg) 30 stretch;
 `;
 
-const Star = styled.p `
+const Stars = styled.p `
     width: 100%;
-    font-size: 36px;
+    font-size: 28px;
     text-align: right;
-    margin: 0 8% 0 0;
+    margin: 1% 8% 1% 0;
     color: rgb(250, 202, 30);
+`;
+
+const StarSpan = styled.span `
+    color: rgb(250, 202, 30);
+    background-color: crimson;
+    border-radius: 60px;
+    padding: 2px 8px;
+    margin: 0 2px;
 `;
 
 const CardImg = styled.img `
     object-fit: scale;
     width: 84%;
-    border: solid 8px transparent;
+    border: solid 10px transparent;
     border-image: url(https://data.whicdn.com/images/220193202/original.jpg) 30 stretch;
 `;
 
@@ -33,7 +41,13 @@ function ImgContainer ({data}) {
     return(
         <Card>
             <CardHeader data={data}/>
-            <Star> &#x2605; &#x2605; &#x2605; &#x2605;  &#x2605; &#x2605;</Star>
+            <Stars>
+                <StarSpan>&#x2605;</StarSpan>
+                <StarSpan>&#x2605;</StarSpan>
+                <StarSpan>&#x2605;</StarSpan>
+                <StarSpan>&#x2605;</StarSpan>
+                <StarSpan>&#x2605;</StarSpan>
+            </Stars>
             <CardImg src={data.url}/>
             <CardPar data={data}/>
         </Card>
